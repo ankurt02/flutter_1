e6475af4a8cf94b088eb6a3df524cc8578b78eed
ankurt02 <ankurt5202@gmail.com>
2023-06-25 02:23:49 +0530

    Day-5 done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_1/pages/home_page.dart';
 import 'package:flutter_1/pages/login_page.dart';
-import 'package:flutter_1/pages/utils/routes.dart';
+import 'package:flutter_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 // main method is the entry point of the code
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
-import 'package:flutter_1/pages/utils/routes.dart';
+import 'package:flutter_1/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
